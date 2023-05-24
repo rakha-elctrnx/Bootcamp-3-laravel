dba5cabf58bba8590029de3257fb99a4f587f1e9
actytauri <bagasrakha.9h@gmail.com>
2023-05-24 14:09:37 +0700

    update sc

--- Pertemuan-6/sc/index.php
+++ Pertemuan-6/sc/index.php
@@ -1,19 +1,16 @@
 <?php
-// !n = n x (n-1)
+// !n = n x (n - 1)
 // !5 = 5 x 4 x 3 x 2 x 1
 // !5 = 120
 
 function faktorial($n) {
-    if ($n <= 1) {
-        return 1;
-    } else {
-        return $n * faktorial($n - 1);
-    }    
+    $result = 1;
+    for ($i = $n; $i > 1; $i--) { 
+        $result *= $i;
+    }
+    return "Nilai dari faktorial $n adalah $result";
 }
 
-$angka = 4;
-$hasil_faktorial = faktorial($angka);
-
-echo "Hasil dari faktorial $angka adalah $hasil_faktorial";
+echo faktorial(6)
 
 ?>
